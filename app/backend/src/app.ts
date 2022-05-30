7514e97f4be2d597f1445855a8109c3a86942ee8
BRUNO <brunofay1@hotmail.com>
2022-05-30 18:46:24 -0300

    NEW implement server configs

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,12 +5,12 @@ class App {
   // ...
 
   constructor() {
-    // ...
+    this.app = express()
     this.config();
     // ...
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
@@ -22,9 +22,9 @@ class App {
     // ..
   }
 
-  // ...
-  public start(PORT: string | number):void {
-    // ...
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Escutando porta ${PORT}`));
   }
 }
 
